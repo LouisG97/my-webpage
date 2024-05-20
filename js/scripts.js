1da686b52d514f990e091d341ae856ff3f1c9554
José Luis Gómez Figueroa <j_k_h@live.com.mx>
2024-05-20 13:21:26 -0600

    Nuevos cambios
    Cambios nuevos

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@
 document.querySelectorAll('header nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start' // Ajuste para alinear el elemento al inicio de la ventana
         });
     });
 });
@@ -16,14 +18,14 @@ window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const headerHeight = header.offsetHeight;
     if (scrollTop > lastScrollTop && scrollTop > headerHeight) {
-        header.style.top = `-${headerHeight}px`; // Hide header
+        header.style.top = `-${headerHeight}px`; // Ocultar encabezado
     } else {
-        header.style.top = '0'; // Show header
+        header.style.top = '0'; // Mostrar encabezado
     }
     lastScrollTop = scrollTop;
 });
 
-// Initialize Swiper
+// Inicializar Swiper
 const swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
